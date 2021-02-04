c09dc0732e902fe00dafbcbff99a28fa4f3e3b8f
sho <s-_-s-_-s.0e1b2i5.python@ezweb.ne.jp>
2021-02-04 21:09:18 +0900

    例外処理、io処理

--- compile10/src/compile10/Main.java
+++ compile10/src/compile10/Main.java
@@ -1,8 +1,15 @@
 package compile10;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
+			String title = "------   a   ------";
+
+
 			int num = 0;
 
 			try {
@@ -41,7 +48,89 @@ public class Main {
 				System.out.println(e);;
 			}
 
+			System.out.println(title.replace("a","10-1"));
+					try {
+						if(args.length == 0) {
+							System.out.println("A");
+						}
+					}catch (NullPointerException e) {
+						System.out.println("B");
+					}finally {
+						System.out.println("C");
+					}
+
+			System.out.println(title.replace("a","10-3"));
+					/*
+					 * Exceptionを拡張したSampleException
+					 * SampleExceptionを拡張したSubSampleException（Exceptionも持っている）
+					 * 1つ目のcatchで全て例外をcatchできてしまうので2つ目のcatchは到達不能となる
+					 *
+					 */
+					try {
+						sample2();
+						sub();
+
+					} catch (Sample2Exception e) {
+						System.out.println("A");
+					}
+					/*
+					catch (SubSampleException e) {
+						System.out.println("B");
+					}
+					*/
+
+			System.out.println(title.replace("a", "10-10"));
+					/*
+					 *  io処理はtry-catch-finallyの外で変数を定義しなければならない
+					 *  なぜなら、tryとfinallyの両方にアクセスしないといけないため
+					 *  仮定
+					 *  tryで処理を入れて、catchで例外の処理を行う
+					 *  finallyで結果的にio処理をクローズさせるため
+					 */
+					FileInputStream is = null;
+					try {
+						is = new FileInputStream("sample.text");
+						throw new FileNotFoundException();
+					} catch (Exception e) {
+						System.out.println("A");
+					}finally {
+						if(is != null) {
+							try {
+								is.close();
+							} catch (IOException e) {
+								throw new RuntimeException();
+							}
+						}
+						System.out.println("B");
+					}
+
+			System.out.println(title.replace("a","10-6"));
+					int result = sample();
+					System.out.println(result);
 
 	}
 
+	private static int sample() {
+		//10−6のメソッド
+		try {
+			throw new RuntimeException();
+		}catch(RuntimeException e) {
+			return 10;
+		}finally {
+			return 20;
+		}
+	}
+	private static void sample2() throws Sample2Exception{
+		throw new Sample2Exception();
+	}
+	private static void sub() throws SubSample2Exception{
+		throw new SubSample2Exception();
+	}
+
+
+
 }
+
+class Sample2Exception extends Exception{}
+class SubSample2Exception extends Sample2Exception{}
+
